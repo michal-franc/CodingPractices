df9c390d05413fcb9b0c1a0ab3b723013490d615
michal-franc <michal.franc@live.com>
2013-06-20 19:43:12 +0200

    all the tests for monkey trouble practice

--- Python/CodingBat/monkey_trouble/monkey_trouble.py
+++ Python/CodingBat/monkey_trouble/monkey_trouble.py
@@ -1,5 +1,5 @@
 def IsTrouble(aSmile, bSmile):
-    if( aSmile and bSmile):
+    if((aSmile and bSmile) or (not aSmile and not bSmile)):
         return True
     else:
         return False
--- Python/CodingBat/monkey_trouble/monkey_trouble_test.py
+++ Python/CodingBat/monkey_trouble/monkey_trouble_test.py
@@ -15,6 +15,23 @@ class MonkeyTrouble(unittest.TestCase):
         #as
         self.assertEqual(isTrouble, actualIsTrouble) 
 
+    def testIsTrouble_WhenBothNotSmile(self):
+        monkeyASmile = False
+        monkeyBSmile = False
+        isTrouble = True
+
+        actualIsTrouble = monkey_trouble.IsTrouble(monkeyASmile, monkeyBSmile)
+
+        self.assertEquals(isTrouble, actualIsTrouble)
+
+    def testIsNotTrouble_WhenOneSmiles(self):
+        monkeyASmile = True
+        monkeyBSmile = False
+        isTrouble = False 
+
+        actualIsTrouble = monkey_trouble.IsTrouble(monkeyASmile, monkeyBSmile)
+
+        self.assertEquals(isTrouble, actualIsTrouble)
 
 if __name__ == "__main__":
     unittest.main()
