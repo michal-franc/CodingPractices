6db5ecaeff10497fefa4ffbd14c4856513d9ec80
michal-franc <michal.franc@live.com>
2013-07-07 13:43:42 +0200

    simplified the code

--- Python/CodingBat/Makes10/makes10.py
+++ Python/CodingBat/Makes10/makes10.py
@@ -1,12 +1,4 @@
 def Do(val1, val2):
     compare = 10
 
-    if(val1 == compare or val2 == compare):
-        return True
-    else:
-        sum = val1 + val2
-
-        if(sum == compare):
-            return True
-        else:
-            return False
+    return (val1 == compare or val2 == compare or val1 + val2 == compare)
