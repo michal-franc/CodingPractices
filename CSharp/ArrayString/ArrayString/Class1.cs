ed01c949b85ea56b95fb96c1e4ab65d8a8b7c8dc
michal-franc <michal.franc@live.com>
2013-07-30 22:40:30 +0200

    Added ArrayStrings practices project

---
+++ CSharp/ArrayString/ArrayString/Class1.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+
+namespace ArrayString
+{
+    public static class StringExtensions
+    {
+        public static char? FirstNonRepeatedChar(this string val)
+        {
+            if (string.IsNullOrWhiteSpace(val)) return null;
+
+            val  = val.Trim();
+
+            if (val.Length <= 1) return val[0];
+
+            var countDictionary = CountChars(val);
+
+            foreach (var keyValue in countDictionary)
+            {
+                if (keyValue.Value <= 1)
+                {
+                    return keyValue.Key;
+                }
+            }
+
+            return null;
+        }
+
+        private static Dictionary<char, int> CountChars(string val)
+        {
+            var countDictionary = new Dictionary<char, int>();
+            foreach (var c in val)
+            {
+                if (c == ' ') continue;
+
+                if (!countDictionary.ContainsKey(c))
+                {
+                    countDictionary.Add(c, 1);
+                }
+                else
+                {
+                    countDictionary[c]++;
+                }
+            }
+
+            return countDictionary;
+        }
+    }
+}
---
+++ CSharp/ArrayString/ArrayStrng.Tests/StringextensionsTests.cs
@@ -0,0 +1,68 @@
+﻿using ArrayString;
+using Xunit;
+using Xunit.Extensions;
+
+namespace ArrayStrng.Tests
+{
+    public class StringextensionsTests
+    {
+        public class FirstNonRepeatedCharacterMethod
+        {
+            [Theory]
+            [InlineData("")]
+            [InlineData(" ")]
+            public void WhenStringEmptyWhite_ReturnNull(string val)
+            {
+               Assert.Null(val.FirstNonRepeatedChar()); 
+            }
+
+            [Fact]
+            public void WhenStringWithSingleChar_ReturnThisChar()
+            {
+                var input = "c";
+
+                var expectedChar = 'c';
+
+                Assert.Equal(expectedChar, input.FirstNonRepeatedChar());
+            }
+
+            [Fact]
+            public void WhenStringWithSingleChar_WithWhiteSpace_ReturnThisChar()
+            {
+                var input = "     c     ";
+
+                var expectedChar = 'c';
+
+                Assert.Equal(expectedChar, input.FirstNonRepeatedChar());
+            }
+            
+            [Fact]
+            public void WhenStringWithNotUniqueChars_Return_Null()
+            {
+                var input = "cc";
+
+                Assert.Null(input.FirstNonRepeatedChar());
+            }
+
+            [Theory]
+            [InlineData("abcd", 'a')]
+            [InlineData("aabccd", 'b')]
+            [InlineData("aabbccdd", null)]
+            [InlineData("abcd", 'a')]
+            [InlineData("xzcsqwe", 'x')]
+            [InlineData("tetter", 'r')]
+            public void SingleWordCase(string inputVal, char? resultChar)
+            {
+               Assert.Equal(resultChar, inputVal.FirstNonRepeatedChar()); 
+            }
+
+            [Theory]
+            [InlineData("aaaaa bcd", 'b')]
+            [InlineData("gogogogoog test", 'e')]
+            public void MultiWordCase(string inputVal, char? resultChar)
+            {
+                Assert.Equal(resultChar, inputVal.FirstNonRepeatedChar());
+            }
+        }
+    }
+}
